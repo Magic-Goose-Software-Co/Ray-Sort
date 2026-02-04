e644da9490f32450094bf95a5ba8b4c23465b09d
zyluxXD <zyluxalt@gmail.com>
2026-02-04 14:19:29 -0600

    Added setuptools, added entrypoint, pyproject.toml, etc

---
+++ src/__main__.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+from pathlib import Path
+import json
+import load
+import mail
+import sort
+
+
+def runApp():
+    config = load.getConfig()
+    account = mail.Account(config["address"], load.getPassword(), config["server"])
+
+    notFoundMailboxes = list(set(config["mailboxes"]) - set(account.mailboxes))
+    for mailbox in notFoundMailboxes:
+        print(f"[ERROR] Expected mailbox \"{mailbox}\" was not found.")
+    if len(notFoundMailboxes) != 0:
+        exit()
+
+    for mailbox in list(set(account.mailboxes) - set(config["mailboxes"])):
+        print(f"[INFO] Mailbox \"{mailbox}\" will not be a sort destination.")
+
+    emails = load.getEmails()
+
+    trainingMail = emails["emails"]
+    lastRun = datetime.strptime(emails["lastRun"], "%Y-%m-%d %H:%M:%S")
+
+    for mailbox in config["mailboxes"]:
+        if mailbox != "INBOX":
+            newMail = account.getMail(mailbox, lastRun)
+            try:
+                trainingMail[mailbox] += newMail
+            except KeyError:
+                trainingMail[mailbox] = newMail
+
+    model = sort.Model(trainingMail)
+
+    if config["sortType"] == "subject":
+        sortFunc = model.sortBySubject
+    elif config["sortType"] == "sender":
+        sortFunc = model.sortBySender
+
+    for email in account.getMail("INBOX", lastRun):
+        destination = sortFunc(email)
+        account.moveEmail(email["uid"], "INBOX", destination)
+        trainingMail[destination].append(email)
+        print("[INFO] Moved \""+email["subject"]+"\" (sender \""+email["sender"]+f"\") to \"{destination}\".")
+
+    with (Path(__file__).resolve().parent / "emails.json").open("w") as emailsFile:
+        json.dump({"lastRun": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "emails": trainingMail}, emailsFile, indent=4)
+
+    account.logout()
+
+
+if __name__ == "__main__":
+    runApp()
---
+++ src/helpers.py
@@ -0,0 +1,63 @@
+import json
+from pathlib import Path
+from datetime import datetime
+
+
+def runSetup():
+    print("")
+    print("What is your email address?")
+    address = input("Address: ")
+
+    print("")
+    print("What is your password?")
+    print("Some mail servers such as iCloud and Gmail require this to be an \"App Specific Password\".")
+    print("This will be stored in the .env file instead of config.json.")
+    password = input("Password: ")
+    print("\033[1APassword: "+"•"*len(password))
+
+    print("")
+    print("What is the IMAP server?")
+    server = input("Server: ")
+
+    print("")
+    print("You should now create any mailboxes you will want Ray to move emails to (if you haven't already done so).")
+    print("Press return when you have created them.")
+    input("")
+
+    import mail
+
+    account = mail.Account(address, password, server)
+
+    mailboxes = []
+    print("Press Y for each of the following mailboxes if you want Ray to move emails to it.")
+    for mailbox in account.mailboxes:
+        if input(mailbox+": (y/n) ").lower() == "y":
+            mailboxes.append(mailbox)
+
+    print("")
+    print("Would you like Ray to sort based on the sender or the subject? ")
+    print("Type \"sender\" or \"subject\" (case-insensitive).")
+    sortType = input("Sort Type: ").lower()
+
+    with (Path(__file__).resolve().parent / "config.json").open("w") as configFile:
+        json.dump({"address": address, "server": server, "mailboxes": mailboxes, "sortType": sortType}, configFile, indent=4)
+
+    with (Path(__file__).resolve().parent / ".env").open("w") as dotEnv:
+        dotEnv.write("PASSWORD="+password)
+
+    with (Path(__file__).resolve().parent / "emails.json").open("w") as emailsFile:
+        json.dump({"lastRun": datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "emails": {}}, emailsFile, indent=4)
+
+    print("")
+    print("Ray is now fully configured and set up! Next time you receive an email, put it in the mailbox you would like Ray to put similar emails in. Run \"python .\" from inside the Ray directory or \"python path/to/ray/dir\" from outside.")
+
+
+def start():
+    base = Path(__file__).resolve().parent
+    config_path = base / "config.json"
+    if not config_path.exists():
+        runSetup()
+    else:
+        import __main__ as app
+        app.runApp()
+
